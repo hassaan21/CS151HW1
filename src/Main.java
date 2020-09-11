2ca27f6899d6e4169934eec93d4fbadd2ed78a0e
hassaan21 <hassaanhaq1998@gmail.com>
2020-09-10 22:54:32 -0700

    animal talking

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		
 		String slashes = "\\";
-		System.out.println("    /|");
-		System.out.println("   ( o>");
-		System.out.println(" )/ |--");
-		System.out.println("((__)" + slashes);
+		System.out.println("    /|          ------------------");
+		System.out.println("   ( o>        /  You've cat to be " + slashes);
+		System.out.println(" )/ |--       <   kitten me         >");
+		System.out.println("((__)" + slashes + "         " + slashes + "  right meow!      /");
+		System.out.println("                ------------------");
 
 	}
 
