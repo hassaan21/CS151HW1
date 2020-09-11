efb6cf3f278a22027587ddc3210f673924dc0300
hassaan21 <hassaanhaq1998@gmail.com>
2020-09-10 22:28:33 -0700

    animal shape

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String slashes = "\\";
+		System.out.println("    /|");
+		System.out.println("   ( o>");
+		System.out.println(" )/ |--");
+		System.out.println("((__)" + slashes);
+
+	}
+
+}
+
+
+/*
+
+    /|
+   ( o>
+ )/ |--
+((__)\
+
+*/
